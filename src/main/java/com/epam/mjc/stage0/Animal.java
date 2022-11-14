a461afdb2bc0e2082c22a3c29c16266175a50529
LoonyJava <loony.java@gmail.com>
2022-11-14 17:50:12 +0500

    Task #1. Class creation

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,24 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String paws;
+        String fur;
+        if (numberOfPaws == 1) paws = "paw";
+        else paws = "paws";
+        if (hasFur == true) fur = "a";
+        else fur = "no";
+        String description = String.format("This animal is mostly %s. It has %d %s and %s fur.", color, numberOfPaws, paws, fur);
+        return description;
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        String description = super.getDescription() + " " + "Moreover, it has 2 wings and can fly.";
+        return description;
+    }
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        Animal animal = new Animal("red",1,false);
+        Dog dog = new Dog();
+        System.out.println(bird.getDescription());
+        System.out.println(animal.getDescription());
+        System.out.println(dog.getDescription());
+    }
 }
